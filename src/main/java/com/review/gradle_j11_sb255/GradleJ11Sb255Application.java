fe8b15ad3f444a42f52d1bbfddab53406d46a259
Kevin Chamorro <kevin,j03@hotmail.com>
2022-04-18 18:05:55 -0500

    Inicio del proyecto

---
+++ src/main/java/com/review/gradle_j11_sb255/GradleJ11Sb255Application.java
@@ -0,0 +1,13 @@
+package com.review.gradle_j11_sb255;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJ11Sb255Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleJ11Sb255Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/review/gradle_j11_sb255/GradleJ11Sb255ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.gradle_j11_sb255;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJ11Sb255ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
